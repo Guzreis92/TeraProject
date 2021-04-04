2e3da83e30448c64a3958600fd651842900bec26
Guzreis92 <78331918+Guzreis92@users.noreply.github.com>
2021-04-04 19:40:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ import joblib
 
 def carregadataset(estado):
     if estado == "São Paulo":
-        return pd.read_csv('datasetfinalsp.csv')
+        return pd.read_csv('data/datasetfinalsp.csv')
     elif estado == "Minas Gerais":
-        return pd.read_csv('datasetfinalmg.csv')
+        return pd.read_csv('data/datasetfinalmg.csv')
     elif estado == "Bahia":
-        return pd.read_csv('datasetfinalba.csv')
+        return pd.read_csv('data/datasetfinalba.csv')
     elif estado == "Rio Grande do Sul":
-        return pd.read_csv('datasetfinalrs.csv')
+        return pd.read_csv('data/datasetfinalrs.csv')
     elif estado == "Goiás":
-        return pd.read_csv('datasetfinalgo.csv')
+        return pd.read_csv('data/datasetfinalgo.csv')
     elif estado == "Pará":
-        return pd.read_csv('datasetfinalpa.csv')
+        return pd.read_csv('data/datasetfinalpa.csv')
 
 #Importação do dataset:
 #dataset = pd.read_csv('data/datasetfinal.csv')
@@ -37,7 +37,8 @@ def app():
     col2.image(foto, use_column_width=True)
     
     #Título do Sidebar
-    st.title("Recomendação de Cesta com Algoritmo Apriori")
+    st.title("Recomendação de Produtos Alimentícios")
+    st.write("Protótipo de recomendação de produtos alimentícios baseado no consumo alimentar da Pesquisa de Orçamentos Familiar 2017-2018.\n Solução desenvolvida para pequenos empreendores utilizando algoritmo Apriori.")
     
     #Subtítulo do Sidebar para Escolha do Estado:
     st.subheader("Selecione o Estado")
@@ -53,19 +54,20 @@ def app():
     
     #Dicionário de Produtos
     product_options = ['-','AGUA', 'MACARRAO', 'ARROZ', 'ACUCAR', 'CAFE COM LEITE', 'MARGARINA COM OU SEM SAL', 'PAO FRANCES',
-                      'BIFE', 'CAFE', 'CARNE BOVINA', 'FRANGO EM PEDACOS']
+                      'BIFE', 'CAFE', 'CARNE BOVINA', 'FRANGO EM PEDACOS', 'AZEITE DE OLIVA', 'OVO DE GALINHA', 'ALFACE',
+                       'PAO DE SAL', 'PAO DE QUEIJO', 'CUSCUZ', 'TOMATE', 'FARINHA DE MANDIOCA', 'CHIMARRAO', 'SUCO', 'REPOLHO']
     
     #Caixa de seleção para cada prduto
     Product1 = st.selectbox("Produto 1:", product_options)
     Product2 = st.selectbox("Produto 2:", product_options)
-    Product3 = st.selectbox("Produto 3:", product_options)
 
     #Subtítlo para Resultado do Apriori
-    st.subheader('Resultado de Recomendação:')
+    st.subheader('Produtos Recomendados:')
     
-    df_final = pd.concat([recomenda(Product1,df_estado),recomenda(Product2,df_estado),recomenda(Product3,df_estado)])
+    df_final = pd.concat([recomenda(Product1,df_estado),recomenda(Product2,df_estado)])
     st.table(df_final.consequents.unique())
     #st.dataframe(df_final)
     
 if __name__ == "__main__":
+    # command to run the app: streamlit run app.py
     app()
